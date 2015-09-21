4eeb974768dd8b399ead72b1cbb7393397da34b0
Cory Todd <cory@pyramidacceptors.com>
2015-09-21 10:47:09 -0700

    Add dead simple sample command line app that uses the singleton approach

---
+++ PyramidNETRS232_TestApp_Simple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidNETRS232_TestApp_Simple
+{
+    class Program
+    {
+        /// <summary>
+        /// Dead simple demo. The error handling and extra features have been omitted for simplicity.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter port name in format: COMX");
+
+            var port = Console.ReadLine();
+
+            SingleBillValidator.Instance.Connect(port);
+
+            Console.WriteLine("Connected on port {0}", port);
+
+            Console.WriteLine("Press ESC to stop");
+            do
+            {
+                while (!Console.KeyAvailable) { }
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Quitting...");
+
+            SingleBillValidator.Instance.Disconnect();
+        }
+    }
+}
---
+++ PyramidNETRS232_TestApp_Simple/SingleBillValidator.cs
@@ -0,0 +1,60 @@
+﻿using PyramidNETRS232;
+using System;
+
+namespace PyramidNETRS232_TestApp_Simple
+{
+    public class SingleBillValidator
+    {
+        private static volatile SingleBillValidator instance;
+        private static object syncRoot = new Object();
+        
+        private PyramidAcceptor validator { get; set; }
+        
+        public RS232Config Config { get; set; }
+
+        public static SingleBillValidator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new SingleBillValidator();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private SingleBillValidator()
+        {
+
+        }
+        
+        public void Connect(string port)
+        {
+            Config = new RS232Config(port, false);
+            validator = new PyramidAcceptor(Config);
+
+            validator.OnCredit += validator_OnCredit;
+
+            validator.Connect();
+        }
+
+        void validator_OnCredit(object sender, CreditArgs e)
+        {
+            Console.WriteLine("Credited bill#: {0}", e.Index);
+        }
+
+        public void Disconnect()
+        {
+            if (validator != null)
+                validator.Close();
+        }
+    }
+}
