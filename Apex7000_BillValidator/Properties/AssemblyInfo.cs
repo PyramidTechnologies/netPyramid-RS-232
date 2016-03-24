410797d4236452a78d76786c7374d74479104833
Cory Todd <cory@pyramidacceptors.com>
2016-03-24 09:51:04 -0700

    Update revision strings

--- Apex7000_BillValidator/Properties/AssemblyInfo.cs
+++ Apex7000_BillValidator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pyramid Technologies Inc")]
 [assembly: AssemblyProduct("NET Pyramid RS-232")]
-[assembly: AssemblyCopyright("© Pyramid Technologies Inc. 2015")]
+[assembly: AssemblyCopyright("© Pyramid Technologies Inc. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly : InternalsVisibleTo("PyramidNETRS232_Test")]
